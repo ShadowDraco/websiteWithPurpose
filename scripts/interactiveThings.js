19a032e93085c3d78de1650ac5915658539ad1fc
Ethan Storm <stormethan10@gmail.com>
2022-09-04 21:45:10 -0700

    Added more to the page
    Images and video for later use, added a feature for revealing more content on a page, added more options for styling and script interactions.

---
+++ scripts/interactiveThings.js
@@ -0,0 +1,32 @@
+
+/* 
+    Click Reveals
+*/
+
+// Elements that can be clicked to reveal other things
+let allClickReveals = document.querySelectorAll('.click-reveal');
+
+function hideHiddenContent(e) {
+    console.log('hidden an element!');
+    e.target.firstElementChild.classList.add('hidden');
+}
+
+function revealHiddenContent(e) {
+
+        // If the element isn't visible show it
+        if (e.target.firstElementChild.classList.contains('hidden')) {
+
+            console.log('revealing a hidden element!');
+            e.target.firstElementChild.classList.remove('hidden');
+        } 
+        // if it is visible hide it
+        else {
+            hideHiddenContent(e);
+        }
+}
+
+// Add event listeners on page load
+allClickReveals.forEach( function(revealer) {
+
+    revealer.addEventListener('click', revealHiddenContent);
+});
